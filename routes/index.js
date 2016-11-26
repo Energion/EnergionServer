f65c05f4a9ae36869338bffc3da047f080186778
Damir Mustafin <frenchthebread@gmail.com>
2016-11-26 15:57:52 +0200

    Route & logic for parsing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import IndexModel from '../models';
 
+import fs from 'fs';
+import tabletojson from 'tabletojson';
+
 const router  = express.Router();
 
 //home
@@ -8,4 +11,81 @@ router.get('/', function(req, res) {
     res.render('index', IndexModel());
 });
 
+router.get('/get-dataset', function (req, res) {
+  fs.readFile('./data/prices.xls', (err, data) => {
+    if (err) throw err;
+    const datatable = data.toString('utf8');
+    const tablesAsJson = tabletojson.convert(datatable);
+    const jsonTable = tablesAsJson[1];
+
+    let days = [];
+    const newDays = removeUnusedKeys(jsonTable);
+    const cleanedHours = cleanHours(newDays);
+
+    for (let i = 0; i < cleanedHours.length; i++) {
+      const hour = cleanedHours[i];
+      for (let dateKey in hour) {
+        const hourVal = hour.hour;
+        if (hour.hasOwnProperty(dateKey)) {
+          if (!arrayHasObejectWithValue(days, dateKey)) {
+            if (dateKey != 'hour') {
+              days.push({
+                date: dateKey,
+                hours: getHours(cleanedHours, dateKey),
+              })
+            }
+          }
+        }
+      }
+    }
+    res.json(days);
+  });
+});
+
 module.exports = router;
+
+function arrayHasObejectWithValue (days, date) {
+  let found = false;
+  for (let i = 0; i < days.length; i++) {
+      if (days[i].date == date) {
+          found = true;
+          break;
+      }
+  }
+  return found;
+}
+
+function removeUnusedKeys (days) {
+  let newDays = [];
+
+  days.forEach((day) => {
+    if (day['0'] != '' && day['0'] != 'Min' && day['0'] != 'Max' &&
+    day['0'] != 'Average' && day['0'] != 'Peak' && day['0'] != 'Off-peak1' &&
+    day['0'] != 'Off-peak2') {
+      newDays.push(day);
+    }
+  })
+
+  return newDays;
+}
+
+function cleanHours (days) {
+  days.forEach((day) => {
+    const hour = day['0'];
+    const splitHours = hour.split('-');
+    day.hour = splitHours[0].trim();
+    delete day['0'];
+  });
+  return days;
+}
+
+function getHours (hours, date) {
+  let newHours = [];
+  for (let i = 0; i < hours.length; i++) {
+    newHours.push({
+      hour: hours[i].hour,
+      price: hours[i][date],
+    })
+  }
+  return newHours;
+}
