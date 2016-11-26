6cde18a540530c10ea6ab36bb284798b3d2f9d48
Damir Mustafin <frenchthebread@gmail.com>
2016-11-27 00:29:58 +0200

    Add test

--- test.js
+++ test.js
@@ -8,6 +8,7 @@ import IndexModel from './models';
 
 const url = config.host + ':' + config.port;
 const errUrl = url + '/random-page';
+const dataUrl = url + '/get-dataset';
 
 function getPageAsElement (url) {
   return got(url)
@@ -20,7 +21,19 @@ function getPageAsElement (url) {
       }
       console.error(err);
     });
-};
+}
+
+function isJSON (something) {
+    if (typeof something != 'string')
+        something = JSON.stringify(something);
+
+    try {
+        JSON.parse(something);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
 
 // OK pages (200)
 test('Page text is correct', async t => {
@@ -39,4 +52,20 @@ test('404 page has correct text', async t => {
 
   t.is($('title').text(), 'Not Found');
   t.is($('h1').text(), 'Not Found');
+});
+
+test('/get-dataset response is correct', async t => {
+
+  const jsonData = await got(dataUrl)
+    .then((res) => {
+      return res.body;
+    })
+    .catch((err) => {
+      if (err.statusCode === 404){
+        return err.response.body;
+      }
+      console.error(err);
+    });
+
+  t.truthy(isJSON(jsonData));
 });
